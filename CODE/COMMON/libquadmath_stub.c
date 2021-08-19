4a81f3c25edd222daaa54ddded44fd6c34cd9c84
Cooper Harasyn <charasyn@outlook.com>
2021-08-19 13:41:57 -0400

    Bypass libquadmath (#7)
    * Avoid linking with libquadmath
    
    Libquadmath is licensed under the LGPL, which is not compatible with
    this project. Therefore, we cannot statically link against it. This adds
    a C file containing stubs for all functions called from libquadmath,
    allowing us to get around the "dependency" on it.
    
    * Fix libquadmath_stub
    
    You can't use __attribute__((alias ...)) on macOS, so we just call the
    lqm_panic function from the stubs.

---
+++ CODE/COMMON/libquadmath_stub.c
@@ -0,0 +1,26 @@
+// libquadmath_stub.c
+// Cooper Harasyn, 2021-08-05
+// This file is required since we aren't allowed to link against
+// libquadmath as that would violate the LGPL that libquadmath is
+// licensed under.
+// Since we don't use any functionality from that library, we have
+// no issues omitting it functionally. However, libgfortran tries to
+// link against certain functions in that library, so we need to
+// provide something for it to link against. Hence, the `lqm_panic`
+// function, which just prints out an error message and then quits the
+// program. This is a good response since the actual libquadmath
+// function isn't linked in, and it should never be called.
+#include <stdlib.h>
+#include <stdio.h>
+const char * const panic_str =
+"==================================================================\n"
+"= Call to a libquadmath function - unexpected coding error!      =\n"
+"= This should never happen! Quitting...                          =\n"
+"==================================================================\n";
+void lqm_panic(void) {
+    fputs(panic_str, stderr);
+    exit(127);
+}
+// Add all undefined libquadmath functions here:
+void quadmath_snprintf() {lqm_panic();}
+void strtoflt128()       {lqm_panic();}
