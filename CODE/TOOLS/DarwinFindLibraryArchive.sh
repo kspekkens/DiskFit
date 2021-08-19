f41159362c2244cb70291906a172a67a65bc0fe3
Cooper Harasyn <charasyn@outlook.com>
2021-08-19 11:09:23 -0700

    Add support for compiling on macOS (Darwin) (#8)
    * Add special compilation support for macOS
    
    On macOS (Darwin), we can't just use "-static" to link the executable,
    since it's impossible to statically link against Apple's libSystem.
    Instead, we need to find all the static object files for all libraries
    that we're using, and manually specify them on the command line so they
    will be used instead of the dynamic versions. The newly added
    DarwinFindLibraryArchive.sh script does this. Modifications were made to
    the Makefile to invoke this script to find the libraries used and to do
    a "semi-static" link where all libraries except libSystem are linked
    statically.
    
    Additionally, some helper shell scripts had to be modified to work with
    non-GNU sed.
    
    * Add -lSystem to link against macOS system library

---
+++ CODE/TOOLS/DarwinFindLibraryArchive.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+function panic {
+    echo "ERROR: $1" >&2
+    exit 1
+}
+function resolve_lib {
+    lib="${1#-l}"
+    oldIFS="$IFS"
+    IFS=":"
+    file=""
+    for dir in $gfort_library_path ; do
+        [ -d "$dir" ] || continue
+        file="$dir/lib$lib.a" ; [ -f "$file" ] && break
+        file="$dir/lib$lib.o" ; [ -f "$file" ] && break
+        file=""
+    done
+    IFS="$oldIFS"
+    if [ -f "$file" ] ; then
+        resolved_lib="$file"
+        return 0
+    else
+        resolved_lib=""
+        return 1
+    fi
+}
+fortest="./fortest.f"
+fortest_exe="./fortest"
+[ -f "${fortest}" ] && panic "${fortest} already exists"
+[ -f "${fortest_exe}" ] && panic "${fortest_exe} already exists"
+
+cat >"${fortest}" <<END
+      end
+END
+gfort_library_path="`gfortran -### -o "${fortest_exe}" "${fortest}" 2>&1 | grep "LIBRARY_PATH" | sed -e s/LIBRARY_PATH=//`"
+gfort_libraries="`gfortran -### --static -o "${fortest_exe}" "${fortest}" 2>&1 | grep "collect" | fmt -1 | grep -e '^ *-l' -e '.a$' | grep -v -e "-lcrt0" -e "-lSystem" -e "-lm" -e "-lquadmath" | fmt -999`"
+
+lib_to_use=""
+for lib in ${gfort_libraries} ; do
+    if ( echo "${lib}" | grep "^-l" >/dev/null ) ; then
+        resolve_lib "${lib}" || panic "Unable to resolve library ${lib}"
+        lib="$resolved_lib"
+    fi
+    lib_to_use="${lib_to_use} ${lib}"
+done
+echo "${lib_to_use}" "${lib_to_use}"
+rm "${fortest}"
--- TESTS/NiceDiffOutput.sh
+++ TESTS/NiceDiffOutput.sh
@@ -2,33 +2,7 @@
 function extract() {
 	file="$1"
 	prefix="$2"
-	cat "${file}" | sed -n -f <( cat <<-SEDSCR
-		# If we match the "start" string, branch forward.
-		/Best fitting values/b mloop
-		# Otherwise, delete line and restart.
-		d
-
-		:mloop
-		# Read in new line
-		n
-
-		# If we have found the end string, quit.
-		/^Degrees of freedom in fit/{q}
-
-		# If line matches any number of things we don't want to print,
-		# ignore it and go back to start of loop.
-		/^-----/{ z ; b mloop }
-		/^# points Dn used in fit/{ z ; b mloop }
-		/^Minimization Details/{ z ; b mloop }
-		# Skip blank lines
-		/^\s*$/{ z ; b mloop }
-
-		# Otherwise, print string and loop
-		s|^|${prefix} |
-		p
-		b mloop
-SEDSCR
-	)
+	cat "${file}" | sed -f NiceDiffOutput.sed | sed -e "s/^/${prefix} /"
 }
 function gen_equals() {
 	count="$1"
