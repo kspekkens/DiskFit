0dbeb78bb9439e03732c16852024cc973274cbfe
Cooper Harasyn <charasyn@outlook.com>
2021-08-29 11:59:43 -0700

    Download using either curl or wget
    wget is not available on macOS by default, but curl is.
    A shell script has been added to check which of the two is
    available, and download a file with either curl or wget.

---
+++ CODE/TOOLS/DownloadFile.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+function panic {
+    echo "ERROR: $1" >&2
+    exit 1
+}
+URL="$1"
+OUTPUT="$2"
+if which curl > /dev/null ; then
+    echo "Downloading using curl..."
+    curl -o "$OUTPUT" "$URL"
+elif which wget > /dev/null ; then
+    echo "Downloading using wget..."
+    wget -O "$OUTPUT" "$URL"
+else
+    panic "Neither curl nor wget installed. Please install one of curl or wget."
+fi
